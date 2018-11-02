eb2bf5c50cf8cb757bb6b3e215318a6b85a95650
jennifer lundberg <jennifer_lundberg@hotmail.com>
2018-11-02 15:24:18 +0100

    added a title

--- Checkpoint4/Checkpoint4/Program.cs
+++ Checkpoint4/Checkpoint4/Program.cs
@@ -8,10 +8,14 @@ namespace Checkpoint4
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Best movies ever!");
+<<<<<<< HEAD
             Console.WriteLine("Trouble of vendetta");
             Console.WriteLine("Reflex Blood");
             Console.WriteLine("Master of Glory");
             Console.WriteLine("Extreme Blood");
+=======
+            Console.WriteLine("Battle of Jeopardy");
+>>>>>>> added a title
         }
     }
 }
