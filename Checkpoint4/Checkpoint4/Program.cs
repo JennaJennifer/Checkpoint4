33567daee8e3fd917315b2454b43112feccb302f
jennifer lundberg <jennifer_lundberg@hotmail.com>
2018-11-02 22:26:40 +0100

    Amended Mtitle to array and sorted by title, print to Console

--- Checkpoint4/Checkpoint4/Program.cs
+++ Checkpoint4/Checkpoint4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Checkpoint4
 {
@@ -6,14 +7,26 @@ namespace Checkpoint4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Best movies ever!");
-            Console.WriteLine("Trouble of vendetta");
-            Console.WriteLine("Reflex Blood");
-            Console.WriteLine("Master of Glory");
-            Console.WriteLine("Extreme Blood");
-            Console.WriteLine("Battle of Jeopardy");
-            Console.WriteLine("Copyright Jennifer 2018");
+            Console.WriteLine("Hello World!\n\nBest movies ever:\n");
+
+            var bestMoviesEver = new Title[]
+            {
+                new Title {MoveTitle = "Trouble of vendetta"},
+                new Title {MoveTitle = "Reflex Blood"},
+                new Title {MoveTitle = "Master of Glory"},
+                new Title {MoveTitle = "Extreme Blood"},
+                new Title {MoveTitle = "Battle of Jeopardy"}
+            };
+
+            var orderBme = bestMoviesEver
+                .OrderBy(e => e.MoveTitle);
+
+            foreach (var title in orderBme)
+            {
+                Console.WriteLine(title.MoveTitle);
+            }
+
+            Console.WriteLine("\nCopyright Jennifer 2018\n");
         }
     }
 }
---
+++ Checkpoint4/Checkpoint4/Title.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Checkpoint4
+{
+    class Title
+    {
+        public string MoveTitle { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"{MoveTitle}";
+        //}
+    }
+}
